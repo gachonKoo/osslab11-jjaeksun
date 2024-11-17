579804c6c5621f6df9617f36381ca940c310ac22
jjaeksun <rkdwndnjs0918@naver.com>
2024-11-17 16:52:08 +0900

    Update tester.py

--- tester.py
+++ tester.py
@@ -1,19 +1,9 @@
 import geo.utils as utils
-import math
-def pythagoras(a, b):
-    c = math.sqrt(a**2 + b**2)
-    return c
-
-def circle(r):
-    area = math.pi * r**2
-    return area
 
 a,b =3,4
-c = pythagoras(a,b)
+c = utils.pythagoras(a,b)
 print('c =',c)
 
 r = 10
-area = circle(r)
+area = utils.circle(r)
 print('area = ',area)
-
-
